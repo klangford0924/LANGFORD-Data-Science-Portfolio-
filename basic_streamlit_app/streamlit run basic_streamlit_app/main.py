e41ec8f099736f87d7582e6fe96921fa4ec3eb8f
klangford0924 <klangfor@nd.edu>
2025-02-08 18:46:14 -0500

    Create main.py

---
+++ basic_streamlit_app/streamlit run basic_streamlit_app/main.py
@@ -0,0 +1,8 @@
+##How to run streamlit app
+
+##The Data set is from Kaggle called WNBA Draft Basketball Player Data (1997 - 2022) from MATT OP. Download that dataset as a csv
+##Here is that link: https://www.kaggle.com/datasets/mattop/wnba-draft-basketball-player-data-1997-2021/data
+
+##Also, make sure to activate streamlit in your terminal by doing a run streamlit command. 
+
+
